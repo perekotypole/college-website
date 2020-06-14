85c04ffa18145e8d808608314ea83e85f7906eb1
Arsen <arsen.palamarchuk@gmail.com>
2020-06-14 16:04:13 +0300

    dateFormat filter fix

--- frontend/src/filters/dateFormat.filter.js
+++ frontend/src/filters/dateFormat.filter.js
@@ -2,7 +2,7 @@ export default function dateFormat(value) {
   const date = value
 
   const day = date.getDate()
-  const month = (date.getMonth() < 10 && '0') + date.getMonth()
+  const month = (date.getMonth() < 11 && '0') + (date.getMonth() + 1)
   const year = date.getFullYear()
 
   return `${day}.${month}.${year}`
